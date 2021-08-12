4e27369cb083c8aa24b64f0be3dfdf31b4b2c1fa
Sumit Jaiswal <sjaiswal@redhat.com>
2021-08-12 15:22:35 +0530

    add new states to IPR

---
+++ plugins/action/deepsec_intrusion_prevention_rules.py
@@ -0,0 +1,335 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Red Hat
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+from ansible.plugins.action import ActionBase
+from ansible.errors import AnsibleActionFail
+from ansible.module_utils.connection import Connection
+
+from ansible_collections.ansible.netcommon.plugins.module_utils.network.common import (
+    utils,
+)
+from ansible_collections.trendmicro.deepsec.plugins.module_utils.deepsec import (
+    map_obj_to_params,
+    map_params_to_obj,
+    remove_get_keys_from_payload_dict,
+)
+from ansible_collections.ansible.utils.plugins.module_utils.common.argspec_validate import (
+    AnsibleArgSpecValidator,
+)
+from ansible_collections.trendmicro.deepsec.plugins.modules.deepsec_intrusion_prevention_rules import (
+    DOCUMENTATION,
+)
+
+
+class ActionModule(ActionBase):
+    """ action module
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(ActionModule, self).__init__(*args, **kwargs)
+        self._result = None
+        self.api_object = "/api/intrusionpreventionrules"
+        self.api_object_search = "/api/intrusionpreventionrules/search"
+        self.api_return = "intrusionPreventionRules"
+        self.module_return = "intrusion_prevention_rules"
+        self.key_transform = {
+            "id": "ID",
+            "minimum_agent_version": "minimumAgentVersion",
+            "application_type_id": "applicationTypeID",
+            "detect_only": "detectOnly",
+            "event_logging_disabled": "eventLoggingDisabled",
+            "generate_event_on_packet_drop": "generateEventOnPacketDrop",
+            "always_include_packet_data": "alwaysIncludePacketData",
+            "debug_mode_enabled": "debugModeEnabled",
+            "original_issue": "originalIssue",
+            "last_updated": "lastUpdated",
+            "can_be_assigned_alone": "canBeAssignedAlone",
+            "case_sensitive": "caseSensitive",
+            "custom_xml": "customXML",
+            "alert_enabled": "alertEnabled",
+            "schedule_id": "scheduleID",
+            "context_id": "contextID",
+            "recommendations_mode": "recommendationsMode",
+            "depends_on_rule_ids": "dependsOnRuleIDs",
+            "cvss_score": "CVSSScore",
+            "cve": "CVE",
+        }
+
+    def _check_argspec(self):
+        aav = AnsibleArgSpecValidator(
+            data=self._task.args,
+            schema=DOCUMENTATION,
+            schema_format="doc",
+            name=self._task.action,
+        )
+        valid, errors, self._task.args = aav.validate()
+        if not valid:
+            self._result["failed"] = True
+            self._result["msg"] = errors
+
+    def _check_for_response_code(self, response_code, response):
+        if response_code >= 400:
+            if response.get("errors"):
+                raise AnsibleActionFail(
+                    "Request failed with HTTPerror code: {0}, and with a response: {1}".format(
+                        response_code, response["errors"]
+                    )
+                )
+            elif response.get("message"):
+                raise AnsibleActionFail(
+                    "Request failed with HTTPerror code: {0}, and with a response: {1}".format(
+                        response_code, response["message"]
+                    )
+                )
+
+    def search_for_intrusion_prevention_rules(
+        self, deepsec_conn_request, search_payload=None
+    ):
+        ipr_obj, ipr_response = deepsec_conn_request.request_method(
+            self.api_object_search, request_method="POST", data=search_payload
+        )
+        self._check_for_response_code(ipr_obj, ipr_response)
+        return ipr_response
+
+    def search_for_ipr_name(self, deepsec_conn_request, search_ipr_by_names):
+        search_result = []
+        if isinstance(search_ipr_by_names, list):
+            for each in search_ipr_by_names:
+                search_payload = {
+                    "maxItems": 1,
+                    "searchCriteria": [
+                        {
+                            "fieldName": "name",
+                            "stringTest": "equal",
+                            "stringValue": each["name"],
+                        }
+                    ],
+                }
+                temp_search_response = self.search_for_intrusion_prevention_rules(
+                    deepsec_conn_request, search_payload
+                )
+                if (
+                    temp_search_response.get("intrusionPreventionRules")
+                    and temp_search_response["intrusionPreventionRules"]
+                ):
+                    search_result.append(
+                        map_obj_to_params(
+                            temp_search_response["intrusionPreventionRules"][
+                                0
+                            ],
+                            self.key_transform,
+                            self.api_return,
+                        )
+                    )
+        else:
+            search_payload = {
+                "maxItems": 1,
+                "searchCriteria": [
+                    {
+                        "fieldName": "name",
+                        "stringTest": "equal",
+                        "stringValue": search_ipr_by_names,
+                    }
+                ],
+            }
+            search_result = self.search_for_intrusion_prevention_rules(
+                deepsec_conn_request, search_payload
+            )
+
+        return search_result
+
+    def reset_module_api_config(
+        self, deepsec_conn_request, module_config_params
+    ):
+        config = {}
+        before = []
+        after = []
+        changed = False
+        for each in module_config_params:
+            search_by_name = self.search_for_ipr_name(
+                deepsec_conn_request, each["name"]
+            )
+            if search_by_name.get(self.api_return):
+                every = map_obj_to_params(
+                    search_by_name[self.api_return][0],
+                    self.key_transform,
+                    self.api_return,
+                )
+                before.append(every)
+                response_code, api_response = deepsec_conn_request.request_method(
+                    "{0}/{1}".format(self.api_object, every["id"]),
+                    request_method="DELETE",
+                    data=each,
+                )
+                self._check_for_response_code(response_code, api_response)
+
+                changed = True
+                if api_response:
+                    after.append(
+                        map_obj_to_params(
+                            api_response, self.key_transform, self.api_return
+                        )
+                    )
+        if changed:
+            config.update({"before": before, "after": after})
+        else:
+            config.update({"before": before})
+        return config, changed
+
+    def configure_module_api(self, deepsec_conn_request, module_config_params):
+        get_supported_keys = ["id", "identifier", "can_be_assigned_alone"]
+        config = {}
+        before = []
+        after = []
+        changed = False
+        remove_from_diff_compare = [
+            "id",
+            "cvss_score",
+            "cve",
+            "can_be_assigned_alone",
+            "type",
+        ]
+        temp_name = []
+        for each in module_config_params:
+            search_by_name = self.search_for_ipr_name(
+                deepsec_conn_request, each["name"]
+            )
+            if search_by_name and search_by_name.get(self.api_return):
+                each_result = search_by_name[self.api_return]
+                for every in each_result:
+                    every = map_obj_to_params(
+                        every, self.key_transform, self.api_return
+                    )
+                    if every["name"] == each["name"]:
+                        each = utils.remove_empties(each)
+                        diff = utils.dict_diff(every, each)
+                if diff:
+                    diff = remove_get_keys_from_payload_dict(
+                        diff, remove_from_diff_compare
+                    )
+                    if diff:
+                        before.append(every)
+                        if self._task.args["state"] == "merged":
+                            # Check for actual modification and if present fire
+                            # the request over that IPR ID
+                            each = utils.remove_empties(
+                                utils.dict_merge(every, each)
+                            )
+                            each = remove_get_keys_from_payload_dict(
+                                each, remove_from_diff_compare
+                            )
+                            changed = True
+                            payload = map_params_to_obj(
+                                each, self.key_transform
+                            )
+                            response_code, api_response = deepsec_conn_request.request_method(
+                                "{0}/{1}".format(self.api_object, every["id"]),
+                                request_method="POST",
+                                data=payload,
+                            )
+                            self._check_for_response_code(
+                                response_code, api_response
+                            )
+                            after.append(
+                                map_obj_to_params(
+                                    api_response,
+                                    self.key_transform,
+                                    self.api_return,
+                                )
+                            )
+                        elif self._task.args["state"] == "replaced":
+                            response_code, api_response = deepsec_conn_request.request_method(
+                                "{0}/{1}".format(self.api_object, every["id"]),
+                                request_method="DELETE",
+                                data=each,
+                            )
+                            self._check_for_response_code(
+                                response_code, api_response
+                            )
+                            changed = True
+                            payload = map_params_to_obj(
+                                each, self.key_transform
+                            )
+                            response_code, api_response = deepsec_conn_request.request_method(
+                                "{0}".format(self.api_object),
+                                request_method="POST",
+                                data=payload,
+                            )
+                            self._check_for_response_code(
+                                response_code, api_response
+                            )
+                            after.append(
+                                map_obj_to_params(
+                                    api_response,
+                                    self.key_transform,
+                                    self.api_return,
+                                )
+                            )
+                    else:
+                        before.append(every)
+                        temp_name.append(every["name"])
+                else:
+                    before.append(every)
+            else:
+                changed = True
+                each = utils.remove_empties(each)
+                each = remove_get_keys_from_payload_dict(
+                    each, get_supported_keys
+                )
+                payload = map_params_to_obj(each, self.key_transform)
+                response_code, api_response = deepsec_conn_request.request_method(
+                    "{0}".format(self.api_object),
+                    request_method="POST",
+                    data=payload,
+                )
+                self._check_for_response_code(response_code, api_response)
+                after.extend(before)
+                after.append(
+                    map_obj_to_params(
+                        api_response, self.key_transform, self.api_return
+                    )
+                )
+        config.update({"before": before, "after": after})
+
+        return config, changed
+
+    def run(self, tmp=None, task_vars=None):
+        self._result = super(ActionModule, self).run(tmp, task_vars)
+        self._check_argspec()
+        if self._result.get("failed"):
+            return self._result
+        deepsec_conn_request = Connection(self._connection._socket_path)
+        if self._task.args["state"] == "gathered":
+            if self._task.args.get("config"):
+                self._result["gathered"] = self.search_for_ipr_name(
+                    deepsec_conn_request, self._task.args["config"]
+                )
+            else:
+                self._result["gathered"] = deepsec_conn_request.request_method(
+                    self.api_object, request_method="GET"
+                )
+        elif (
+            self._task.args["state"] == "merged"
+            or self._task.args["state"] == "replaced"
+        ):
+            if self._task.args.get("config"):
+                self._result[self.module_return], self._result[
+                    "changed"
+                ] = self.configure_module_api(
+                    deepsec_conn_request, self._task.args["config"]
+                )
+        elif self._task.args["state"] == "deleted":
+            if self._task.args.get("config"):
+                self._result[self.module_return], self._result[
+                    "changed"
+                ] = self.reset_module_api_config(
+                    deepsec_conn_request, self._task.args["config"]
+                )
+
+        return self._result
--- plugins/modules/deepsec_intrusion_prevention_rules.py
+++ plugins/modules/deepsec_intrusion_prevention_rules.py
@@ -3,19 +3,14 @@
 # Copyright 2021 Red Hat
 # GNU General Public License v3.0+
 # (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+"""
+The module file for deepsec_intrusion_preventionrules
+"""
 
 from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
-
-
-ANSIBLE_METADATA = {
-    "metadata_version": "1.1",
-    "status": ["preview"],
-    "supported_by": "community",
-}
-
-DOCUMENTATION = """
+DOCUMENTATION = r"""
 module: deepsec_intrusion_preventionrules
 short_description: Create a new intrusion prevention rule.
 description:
@@ -161,22 +156,23 @@ options:
         the value is returned in the I(gathered) key within the result.
     type: str
     choices:
-      - present
-      - absent
+      - merged
+      - replaced
+      - deleted
       - gathered
     default: present
 
 author: Ansible Security Automation Team (@justjais) <https://github.com/ansible-security>"
 """
 
-EXAMPLES = """
+EXAMPLES = r"""
 
-# Using PRESENT state
+# Using MERGED state
 # -------------------
 
 - name: Create Intrusion Prevention Rules
   trendmicro.deepsec.deepsec_intrusion_preventionrules:
-    state: present
+    state: merged
     config:
       - alert_enabled: false
         always_include_packet_data: false
@@ -252,7 +248,7 @@ EXAMPLES = """
 
 - name: Modify the severity of Integrity Monitoring Rule by name
   trendmicro.deepsec.deepsec_intrusion_preventionrules:
-    state: present
+    state: merged
     config:
       - name: TEST IPR 2
         severity: low
@@ -359,12 +355,12 @@ EXAMPLES = """
   trendmicro.deepsec.deepsec_intrusion_preventionrules:
     state: gathered
 
-# Using ABSENT state
+# Using DELETED state
 # ------------------
 
 - name: Delete Intrusion Prevention Rules
   trendmicro.deepsec.deepsec_intrusion_preventionrules:
-    state: absent
+    state: deleted
     config:
       - name: TEST IPR 1
       - name: TEST IPR 2
@@ -415,287 +411,3 @@ EXAMPLES = """
 # }
 
 """
-
-from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.trendmicro.deepsec.plugins.module_utils.deepsec import (
-    DeepSecurityRequest,
-    map_obj_to_params,
-    map_params_to_obj,
-    remove_get_keys_from_payload_dict,
-)
-from ansible_collections.ansible.netcommon.plugins.module_utils.network.common import (
-    utils,
-)
-
-key_transform = {
-    "id": "ID",
-    "minimum_agent_version": "minimumAgentVersion",
-    "application_type_id": "applicationTypeID",
-    "detect_only": "detectOnly",
-    "event_logging_disabled": "eventLoggingDisabled",
-    "generate_event_on_packet_drop": "generateEventOnPacketDrop",
-    "always_include_packet_data": "alwaysIncludePacketData",
-    "debug_mode_enabled": "debugModeEnabled",
-    "original_issue": "originalIssue",
-    "last_updated": "lastUpdated",
-    "can_be_assigned_alone": "canBeAssignedAlone",
-    "case_sensitive": "caseSensitive",
-    "custom_xml": "customXML",
-    "alert_enabled": "alertEnabled",
-    "schedule_id": "scheduleID",
-    "context_id": "contextID",
-    "recommendations_mode": "recommendationsMode",
-    "depends_on_rule_ids": "dependsOnRuleIDs",
-    "cvss_score": "CVSSScore",
-    "cve": "CVE",
-}
-
-get_supported_keys = ["id", "identifier", "can_be_assigned_alone"]
-
-api_object = "/api/intrusionpreventionrules"
-api_object_search = "/api/intrusionpreventionrules/search"
-api_return = "intrusionPreventionRules"
-module_return = "intrusion_preventionrules"
-
-
-def search_for_ipr_by_name(deepsec_request, name):
-    search_payload = {
-        "maxItems": 1,
-        "searchCriteria": [
-            {"fieldName": "name", "stringTest": "equal", "stringValue": name}
-        ],
-    }
-    search_result = search_for_intrusion_prevention_rules(
-        deepsec_request, search_payload
-    )
-    return search_result
-
-
-def display_gathered_result(module, deepsec_request):
-    return_config = {}
-    if module.params.get("config"):
-        return_config["config"] = []
-        for each in module.params.get("config"):
-            search_result = search_for_ipr_by_name(
-                deepsec_request, each["name"]
-            )
-            return_config["config"].extend(
-                map_obj_to_params(search_result, key_transform, api_return)[
-                    api_return
-                ]
-            )
-    else:
-        search_result = search_for_intrusion_prevention_rules(deepsec_request)
-        return_config["config"] = map_obj_to_params(
-            search_result, key_transform, api_return
-        )[api_return]
-    module.exit_json(gathered=return_config["config"], changed=False)
-
-
-def search_for_intrusion_prevention_rules(
-    deepsec_api_request, search_payload=None
-):
-    search_for_intrusion_prevention_rules = deepsec_api_request.post(
-        api_object_search, data=search_payload
-    )
-    return search_for_intrusion_prevention_rules
-
-
-def reset_module_api_config(module, deepsec_request):
-    if module.params.get("config"):
-        config = {}
-        before = []
-        after = []
-        changed = False
-        for each in module.params["config"]:
-            search_by_name = search_for_ipr_by_name(
-                deepsec_request, each["name"]
-            )
-            if search_by_name.get(api_return):
-                every = map_obj_to_params(
-                    search_by_name[api_return][0], key_transform, api_return
-                )
-                before.append(every)
-                api_request = deepsec_request.delete(
-                    "{0}/{1}".format(api_object, every["id"]), data=each
-                )
-                if api_request.get("errors"):
-                    module.fail_json(msg=api_request["errors"])
-                elif api_request.get("message"):
-                    module.fail_json(msg=api_request["message"])
-                changed = True
-                if api_request:
-                    after.append(
-                        map_obj_to_params(
-                            api_request, key_transform, api_return
-                        )
-                    )
-        if changed:
-            config.update({"before": before, "after": after})
-            module.exit_json(intrusion_preventionrules=config, changed=changed)
-        else:
-            config.update({"before": before})
-            module.exit_json(intrusion_preventionrules=config, changed=changed)
-
-
-def configure_module_api(argspec, module, deepsec_request):
-    if module.params.get("config"):
-        config = {}
-        before = []
-        after = []
-        changed = False
-        remove_from_diff_compare = [
-            "cvss_score",
-            "cve",
-            "can_be_assigned_alone",
-            "type",
-        ]
-        temp_name = []
-        for each in module.params["config"]:
-            search_by_name = search_for_ipr_by_name(
-                deepsec_request, each["name"]
-            )
-            if search_by_name.get(api_return):
-                each_result = search_by_name[api_return]
-                for every in each_result:
-                    every = map_obj_to_params(every, key_transform, api_return)
-                    if every["name"] == each["name"]:
-                        diff = utils.dict_diff(every, each)
-                if diff:
-                    diff = remove_get_keys_from_payload_dict(
-                        diff, remove_from_diff_compare
-                    )
-                    if diff:
-                        if each["name"] not in temp_name:
-                            after.extend(before)
-                        before.append(every)
-                        # Check for actual modification and if present fire
-                        # the request over that IPR ID
-                        each = utils.remove_empties(
-                            utils.dict_merge(every, each)
-                        )
-                        each = remove_get_keys_from_payload_dict(
-                            each, remove_from_diff_compare
-                        )
-                        changed = True
-                        utils.validate_config(argspec, {"config": [each]})
-                        payload = map_params_to_obj(each, key_transform)
-                        api_request = deepsec_request.post(
-                            "{0}/{1}".format(api_object, every["id"]),
-                            data=payload,
-                        )
-                        if api_request.get("errors"):
-                            module.fail_json(msg=api_request["errors"])
-                        elif api_request.get("message"):
-                            module.fail_json(msg=api_request["message"])
-                        after.append(
-                            map_obj_to_params(
-                                api_request, key_transform, api_return
-                            )
-                        )
-                    else:
-                        before.append(every)
-                        temp_name.append(every["name"])
-                else:
-                    before.append(every)
-            else:
-                changed = True
-                each = remove_get_keys_from_payload_dict(
-                    each, get_supported_keys
-                )
-                utils.validate_config(argspec, {"config": [each]})
-                payload = map_params_to_obj(each, key_transform)
-                api_request = deepsec_request.post(
-                    "{0}".format(api_object), data=payload
-                )
-                if api_request.get("errors"):
-                    module.fail_json(msg=api_request["errors"])
-                elif api_request.get("message"):
-                    module.fail_json(msg=api_request["message"])
-                after.append(
-                    map_obj_to_params(api_request, key_transform, api_return)
-                )
-        config.update({"before": before, "after": after})
-        module.exit_json(intrusion_preventionrules=config, changed=changed)
-
-
-def main():
-
-    ipr_spec = {
-        "name": dict(type="str"),
-        "description": dict(type="str"),
-        "minimum_agent_version": dict(type="str"),
-        "application_type_id": dict(type="int"),
-        "priority": dict(
-            type="str", choices=["lowest", "low", "normal", "high", "highest"]
-        ),
-        "severity": dict(
-            type="str", choices=["low", "medium", "high", "critical"]
-        ),
-        "detect_only": dict(type="bool"),
-        "event_logging_disabled": dict(type="bool"),
-        "generate_event_on_packet_drop": dict(type="bool"),
-        "always_include_packet_data": dict(type="bool"),
-        "debug_mode_enabled": dict(type="bool"),
-        "type": dict(
-            type="str",
-            choices=[
-                "custom",
-                "smart",
-                "vulnerability",
-                "exploit",
-                "hidden",
-                "policy",
-                "info",
-            ],
-        ),
-        "original_issue": dict(type="int"),
-        "id": dict(type="int"),
-        "identifier": dict(type="str"),
-        "last_updated": dict(type="int"),
-        "template": dict(
-            type="str", choices=["signature", "start-end-patterns", "custom"]
-        ),
-        "signature": dict(type="str"),
-        "start": dict(type="str"),
-        "patterns": dict(type="list", elements="str"),
-        "end": dict(type="str"),
-        "can_be_assigned_alone": dict(type="bool"),
-        "case_sensitive": dict(type="bool"),
-        "condition": dict(type="str", choices=["all", "any", "none"]),
-        "action": dict(type="str", choices=["drop", "log-only"]),
-        "custom_xml": dict(type="str"),
-        "alert_enabled": dict(type="bool"),
-        "schedule_id": dict(type="int"),
-        "context_id": dict(type="int"),
-        "recommendations_mode": dict(
-            type="str", choices=["enabled", "ignored", "unknown", "disabled"]
-        ),
-        "depends_on_rule_ids": dict(type="list", elements="int"),
-        "cvss_score": dict(type="str"),
-        "cve": dict(type="list", elements="str"),
-    }
-
-    argspec = dict(
-        state=dict(
-            choices=["present", "absent", "gathered"], default="present"
-        ),
-        config=dict(type="list", elements="dict", options=ipr_spec),
-    )
-
-    module = AnsibleModule(argument_spec=argspec, supports_check_mode=True)
-    deepsec_request = DeepSecurityRequest(module)
-    module.params = utils.remove_empties(module.params)
-
-    if module.params["state"] == "gathered":
-        display_gathered_result(module=module, deepsec_request=deepsec_request)
-    elif module.params["state"] == "absent":
-        reset_module_api_config(module=module, deepsec_request=deepsec_request)
-    elif module.params["state"] == "present":
-        configure_module_api(
-            argspec=argspec, module=module, deepsec_request=deepsec_request
-        )
-
-
-if __name__ == "__main__":
-    main()

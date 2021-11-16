df95be536f35b0a14c709903f1cba2e80c743ec1
Sumit Jaiswal <sjaiswal@redhat.com>
2021-11-16 12:10:42 +0530

    revert comment

--- tests/unit/mock/loader.py
+++ tests/unit/mock/loader.py
@@ -46,12 +46,12 @@ class DictDataLoader(DataLoader):
 
     # TODO: the real _get_file_contents returns a bytestring, so we actually convert the
     #       unicode/text it's created with to utf-8
-    # def _get_file_contents(self, path):
-    #     path = to_text(path)
-    #     if path in self._file_mapping:
-    #         return (to_bytes(self._file_mapping[path]), False)
-    #     else:
-    #         raise AnsibleParserError("file not found: %s" % path)
+    def _get_file_contents(self, path):
+        path = to_text(path)
+        if path in self._file_mapping:
+            return (to_bytes(self._file_mapping[path]), False)
+        else:
+            raise AnsibleParserError("file not found: %s" % path)
 
     def path_exists(self, path):
         path = to_text(path)

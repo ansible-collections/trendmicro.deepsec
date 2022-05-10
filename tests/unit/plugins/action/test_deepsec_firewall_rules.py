fd4c43204b97d113c721df67c8d7f1cdfcad2f05
Sumit Jaiswal <sjaiswal@redhat.com>
2022-05-10 23:55:03 +0530

    fix UTC

--- tests/unit/plugins/action/test_deepsec_firewall_rules.py
+++ tests/unit/plugins/action/test_deepsec_firewall_rules.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018 Red Hat
+# Copyright (c) 2022 Red Hat
 #
 # This file is part of Ansible
 #
@@ -20,46 +20,91 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-import os
-import pytest
+import unittest
+import tempfile
 from ansible.playbook.task import Task
 from ansible.template import Templar
 from ansible_collections.trendmicro.deepsec.plugins.action.deepsec_firewall_rules import (
     ActionModule,
 )
-from ansible_collections.trendmicro.deepsec.tests.unit.plugins.modules.utils import (
-    set_module_args,
-    exit_json,
-    fail_json,
-    AnsibleFailJson,
-    AnsibleExitJson
-)
-from ansible_collections.trendmicro.deepsec.tests.unit.compat.mock import (
+from ansible_collections.ansible.utils.tests.unit.compat.mock import (
     MagicMock,
     patch,
 )
 
-from ansible_collections.trendmicro.deepsec.tests.unit.mock.loader import (
-    DictDataLoader,
-    patch,
-)
+RESPONSE_PAYLOAD = {
+    "firewallRules": [
+        {
+            "action": "deny",
+            "priority": "0",
+            "direction": "incoming",
+            "description": "incoming firewall 1 rule description",
+            "frameType": "ip",
+            "frameNumber": 2048,
+            "frameNot": False,
+            "protocol": "tcp",
+            "protocolNot": False,
+            "sourceIPType": "any",
+            "sourceIPNot": False,
+            "sourceMACType": "any",
+            "sourceMACNot": False,
+            "sourcePortType": "any",
+            "sourcePortNot": False,
+            "destinationIPType": "any",
+            "destinationIPNot": False,
+            "destinationMACType": "any",
+            "destinationMACNot": False,
+            "destinationPortType": "any",
+            "destinationPortNot": False,
+            "anyFlags": True,
+            "logDisabled": True,
+            "includePacketData": False,
+            "alertEnabled": False,
+            "ID": 146,
+            "name": "test_firewallrule_1",
+        }
+    ]
+}
 
-from ansible.module_utils import basic
-from ansible_collections.trendmicro.deepsec.plugins.modules import deepsec_firewall_rules
+REQUEST_PAYLOAD = [
+    {
+        "name": "test_firewallrule_1",
+        "description": "incoming firewall 1 rule description",
+        "action": "deny",
+        "priority": 0,
+        "source_iptype": "any",
+        "destination_iptype": "any",
+        "direction": "incoming",
+        "protocol": "tcp",
+        "log_disabled": True,
+    },
+    {
+        "name": "test_firewallrule_2",
+        "description": "incoming firewall 2 rule description",
+        "action": "deny",
+        "priority": 0,
+        "source_iptype": "any",
+        "source_ipnot": False,
+        "source_port_type": "any",
+        "destination_iptype": "any",
+        "direction": "incoming",
+        "protocol": "tcp",
+    },
+]
 
-OBJECT = {'layer': 'foo', 'position': 'bar', 'name': 'baz',
-          'source': [{'name': 'lol'}], 'destination': [{'name': 'Any'}],
-          'action': {'name': 'drop'}, 'enabled': True}
-PAYLOAD = {'layer': 'foo', 'position': 'bar', 'name': 'baz'}
 
-
-class TestDeepsecFirewallRules(object):
+class TestDeepsecFirewallRules(unittest.TestCase):
     def setUp(self):
         task = MagicMock(Task)
+        # Ansible > 2.13 looks for check_mode in task
+        task.check_mode = False
         play_context = MagicMock()
+        # Ansible <= 2.13 looks for check_mode in play_context
         play_context.check_mode = False
-        connection = MagicMock()
-        fake_loader = DictDataLoader({})
+        connection = patch(
+            "ansible_collections.trendmicro.deepsec.plugins.action.deepsec_firewall_rules.Connection"
+        )
+        fake_loader = {}
         templar = Templar(loader=fake_loader)
         self._plugin = ActionModule(
             task=task,
@@ -70,91 +115,190 @@ class TestDeepsecFirewallRules(object):
             shared_loader_obj=None,
         )
         self._plugin._task.action = "deepsec_firewall_rules"
+        self._task_vars = {}
 
-    @staticmethod
-    def _load_fixture(filename):
-        """Load a fixture from the filesystem
-        :param filename: The name of the file to load
-        :type filename: str
-        :return: The file contents
-        :rtype: str
-        """
-        fixture_name = os.path.join(
-            os.path.dirname(__file__), "fixtures", filename
+    @patch("ansible.module_utils.connection.Connection.__rpc__")
+    def test_deepsec_firewall_rules_merged(self, connection):
+        self._plugin.search_for_resource_name = MagicMock()
+        self._plugin.search_for_resource_name.return_value = {}
+        self._plugin._connection.socket_path = (
+            tempfile.NamedTemporaryFile().name
         )
-        with open(fixture_name) as fhand:
-            return fhand.read()
-    
-    def test_asa_acls_merged(self):
-        
+        self._plugin._connection._shell = MagicMock()
+        self._plugin._task.args = {
+            "state": "merged",
+            "config": REQUEST_PAYLOAD,
+        }
+        result = self._plugin.run(task_vars=self._task_vars)
+        self.assertTrue(result["changed"])
 
-    # @pytest.fixture(autouse=True)
-    # def module_mock(self, mocker):
-    #     return mocker.patch.multiple(basic.AnsibleModule, exit_json=exit_json, fail_json=fail_json)
+    @patch("ansible.module_utils.connection.Connection.__rpc__")
+    def test_deepsec_firewall_rules_merged_idempotent(self, connection):
+        self._plugin._connection.socket_path = (
+            tempfile.NamedTemporaryFile().name
+        )
+        self._plugin._connection._shell = MagicMock()
+        self._plugin.search_for_resource_name = MagicMock()
+        self._plugin.search_for_resource_name.return_value = RESPONSE_PAYLOAD
+        self._plugin.api_return = "firewallRules"
+        self._plugin._task.args = {
+            "state": "merged",
+            "config": [
+                {
+                    "name": "test_firewallrule_1",
+                    "description": "incoming firewall 1 rule description",
+                    "action": "deny",
+                    "priority": 0,
+                    "source_iptype": "any",
+                    "destination_iptype": "any",
+                    "direction": "incoming",
+                    "protocol": "tcp",
+                    "log_disabled": True,
+                }
+            ],
+        }
+        result = self._plugin.run(task_vars=self._task_vars)
+        self.assertFalse(result["changed"])
 
-    # @pytest.fixture
-    # def connection_mock(self, mocker):
-    #     connection_class_mock = mocker.patch('ansible_collections.check_point.mgmt.plugins.modules.checkpoint_access_rule.Connection')
-    #     return connection_class_mock.return_value
+    @patch("ansible.module_utils.connection.Connection.__rpc__")
+    def test_deepsec_firewall_rules_replaced(self, connection):
+        self._plugin._connection.socket_path = (
+            tempfile.NamedTemporaryFile().name
+        )
+        self._plugin._connection._shell = MagicMock()
+        self._plugin.search_for_resource_name = MagicMock()
+        self._plugin.search_for_resource_name.return_value = RESPONSE_PAYLOAD
+        self._plugin.api_return = "firewallRules"
+        self._plugin._task.args = {
+            "state": "replaced",
+            "config": [
+                {
+                    "name": "test_firewallrule_1",
+                    "description": "outgoing firewall 1 replaced rule",
+                    "action": "deny",
+                    "priority": 0,
+                    "source_iptype": "any",
+                    "destination_iptype": "any",
+                    "direction": "outgoing",
+                    "protocol": "tcp",
+                    "log_disabled": True,
+                }
+            ],
+        }
+        result = self._plugin.run(task_vars=self._task_vars)
+        self.assertTrue(result["changed"])
 
-    # @pytest.fixture
-    # def get_access_rule_200(self, mocker):
-    #     mock_function = mocker.patch('ansible_collections.check_point.mgmt.plugins.modules.checkpoint_access_rule.get_access_rule')
-    #     mock_function.return_value = (200, OBJECT)
-    #     return mock_function.return_value
+    @patch("ansible.module_utils.connection.Connection.__rpc__")
+    def test_deepsec_firewall_rules_replaced_idempotent(self, connection):
+        self._plugin._connection.socket_path = (
+            tempfile.NamedTemporaryFile().name
+        )
+        self._plugin._connection._shell = MagicMock()
+        self._plugin.search_for_resource_name = MagicMock()
+        self._plugin.search_for_resource_name.return_value = {
+            "firewallRules": [
+                {
+                    "action": "deny",
+                    "priority": "0",
+                    "direction": "outgoing",
+                    "description": "outgoing firewall 1 replaced rule",
+                    "frameType": "ip",
+                    "frameNumber": 2048,
+                    "frameNot": False,
+                    "protocol": "tcp",
+                    "protocolNot": False,
+                    "sourceIPType": "any",
+                    "sourceIPNot": False,
+                    "sourceMACType": "any",
+                    "sourceMACNot": False,
+                    "sourcePortType": "any",
+                    "sourcePortNot": False,
+                    "destinationIPType": "any",
+                    "destinationIPNot": False,
+                    "destinationMACType": "any",
+                    "destinationMACNot": False,
+                    "destinationPortType": "any",
+                    "destinationPortNot": False,
+                    "anyFlags": True,
+                    "logDisabled": True,
+                    "includePacketData": False,
+                    "alertEnabled": False,
+                    "ID": 147,
+                    "name": "test_firewallrule_1",
+                }
+            ]
+        }
+        self._plugin.api_return = "firewallRules"
+        self._plugin._task.args = {
+            "state": "replaced",
+            "config": [
+                {
+                    "name": "test_firewallrule_1",
+                    "description": "outgoing firewall 1 replaced rule",
+                    "action": "deny",
+                    "priority": 0,
+                    "source_iptype": "any",
+                    "destination_iptype": "any",
+                    "direction": "outgoing",
+                    "protocol": "tcp",
+                    "log_disabled": True,
+                }
+            ],
+        }
+        result = self._plugin.run(task_vars=self._task_vars)
+        self.assertFalse(result["changed"])
 
-    # @pytest.fixture
-    # def get_access_rule_404(self, mocker):
-    #     mock_function = mocker.patch('ansible_collections.check_point.mgmt.plugins.modules.checkpoint_access_rule.get_access_rule')
-    #     mock_function.return_value = (404, 'Object not found')
-    #     return mock_function.return_value
+    @patch("ansible.module_utils.connection.Connection.__rpc__")
+    def test_deepsec_firewall_rules_deleted(self, connection):
+        self._plugin._connection.socket_path = (
+            tempfile.NamedTemporaryFile().name
+        )
+        self._plugin._connection._shell = MagicMock()
+        self._plugin.search_for_resource_name = MagicMock()
+        self._plugin.search_for_resource_name.return_value = RESPONSE_PAYLOAD
+        self._plugin.api_return = "firewallRules"
+        self._plugin._task.args = {
+            "state": "deleted",
+            "config": [
+                {
+                    "name": "test_firewallrule_1",
+                }
+            ],
+        }
+        result = self._plugin.run(task_vars=self._task_vars)
+        self.assertTrue(result["changed"])
 
-    # def test_create(self, get_access_rule_404, connection_mock):
-    #     connection_mock.send_request.return_value = (200, OBJECT)
-    #     result = self._run_module(PAYLOAD)
+    @patch("ansible.module_utils.connection.Connection.__rpc__")
+    def test_deepsec_firewall_rules_deleted_idempotent(self, connection):
+        self._plugin.search_for_resource_name = MagicMock()
+        self._plugin.search_for_resource_name.return_value = {}
+        self._plugin._connection.socket_path = (
+            tempfile.NamedTemporaryFile().name
+        )
+        self._plugin._connection._shell = MagicMock()
+        self._plugin._task.args = {
+            "state": "deleted",
+            "config": [
+                {
+                    "name": "test_firewallrule_1",
+                }
+            ],
+        }
+        result = self._plugin.run(task_vars=self._task_vars)
+        self.assertFalse(result["changed"])
 
-    #     assert result['changed']
-    #     assert 'checkpoint_access_rules' in result
-
-    # def test_create_idempotent(self, get_access_rule_200, connection_mock):
-    #     connection_mock.send_request.return_value = (200, PAYLOAD)
-    #     result = self._run_module(PAYLOAD)
-
-    #     assert not result['changed']
-
-    # def test_update(self, get_access_rule_200, connection_mock):
-    #     payload_for_update = {'enabled': False}
-    #     payload_for_update.update(PAYLOAD)
-    #     connection_mock.send_request.return_value = (200, payload_for_update)
-    #     result = self._run_module(payload_for_update)
-
-    #     assert result['changed']
-    #     assert not result['checkpoint_access_rules']['enabled']
-
-    # def test_delete(self, get_access_rule_200, connection_mock):
-    #     connection_mock.send_request.return_value = (200, OBJECT)
-    #     payload_for_delete = {'state': 'absent'}
-    #     payload_for_delete.update(PAYLOAD)
-    #     result = self._run_module(payload_for_delete)
-
-    #     assert result['changed']
-
-    # def test_delete_idempotent(self, get_access_rule_404, connection_mock):
-    #     payload = {'name': 'baz', 'state': 'absent'}
-    #     connection_mock.send_request.return_value = (200, OBJECT)
-    #     result = self._run_module(payload)
-
-    #     assert not result['changed']
-
-    # def _run_module(self, module_args):
-    #     set_module_args(module_args)
-    #     with pytest.raises(AnsibleExitJson) as ex:
-    #         self.module.main()
-    #     return ex.value.args[0]
-
-    # def _run_module_with_fail_json(self, module_args):
-    #     set_module_args(module_args)
-    #     with pytest.raises(AnsibleFailJson) as exc:
-    #         self.module.main()
-    #     result = exc.value.args[0]
-    #     return result
+    @patch("ansible.module_utils.connection.Connection.__rpc__")
+    def test_deepsec_firewall_rules_gathered(self, connection):
+        self._plugin._connection.socket_path = (
+            tempfile.NamedTemporaryFile().name
+        )
+        self._plugin._connection._shell = MagicMock()
+        self._plugin.search_for_resource_name = MagicMock()
+        self._plugin.search_for_resource_name.return_value = RESPONSE_PAYLOAD
+        self._plugin.api_return = "firewallRules"
+        self._plugin._task.args = {
+            "state": "gathered",
+            "config": [{"name": "test_firewallrule_1"}],
+        }
+        result = self._plugin.run(task_vars=self._task_vars)
+        self.assertFalse(result["changed"])
